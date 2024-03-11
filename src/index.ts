c3dbade931836844279a1cb4578e0f55384e5aa8
Andrew Chernoff <whitedrew538@gmail.com>
2024-03-11 20:51:32 +0500

    changed root to uploads folder in /uploads endpoint

--- src/index.ts
+++ src/index.ts
@@ -50,10 +50,9 @@ app.use(
 app.use('/uploads', express.static('uploads'))
 
 app.post("/upload", upload.single('image'), (req: Request, res: Response) => {
-  debugger
   if(req.file) {
   return res.status(200).json({
-    url: `/${req.file.path}`/* `/uploads/${req.file.originalname}` */
+    url: /* `/${req.file.path}` */`/uploads/${req.file.originalname}`
   })
   } else {
     return res.status(500)
